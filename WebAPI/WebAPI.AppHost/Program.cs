a1cd5d1c2b8c480e3fbad5534e851b32aba6a81a
俊霖 <testaurora@hotmail.com>
2025-02-04 16:41:25 +0800

    增加WebAPI的依賴與組態資訊

--- WebAPI/WebAPI.AppHost/Program.cs
+++ WebAPI/WebAPI.AppHost/Program.cs
@@ -6,6 +6,18 @@ var postgres = builder
     .WithPgAdmin() // 執行個體網頁客戶端-管理工具
     .WithPgWeb(); // 資料庫網頁客戶端-資料表查詢工具
 
-builder.AddProject<Projects.WebAPI>("webapi");
+// 編排：添加資料庫的監控、連線字串傳遞(給PgWeb)
+var 震旦雲端事業部 = postgres.AddDatabase("a0ac");
+var 雲端人資系統展示平台 = postgres.AddDatabase("oacloud");
+var 汎美 = postgres.AddDatabase("gm9415");
+
+builder.AddProject<Projects.WebAPI>("webapi")
+                            // 服務間的關聯性
+                            .WithReference(震旦雲端事業部) // 服務的相依性、環境參數傳遞
+                                .WaitFor(震旦雲端事業部) // 啟動的相依性
+                            .WithReference(雲端人資系統展示平台)
+                                .WaitFor(雲端人資系統展示平台)
+                            .WithReference(汎美)
+                                .WaitFor(汎美);
 
 builder.Build().Run();
