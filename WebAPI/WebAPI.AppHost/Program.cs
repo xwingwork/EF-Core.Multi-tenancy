d9d30b8aea1ed2aab0f144f5b7331507647ae5f4
俊霖 <testaurora@hotmail.com>
2025-02-04 15:30:01 +0800

    增加SQLServer跟SQLClient

--- WebAPI/WebAPI.AppHost/Program.cs
+++ WebAPI/WebAPI.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder
+    .AddPostgres("postgres") // 執行個體，包含預設資料庫
+    .WithPgAdmin() // 執行個體網頁客戶端-管理工具
+    .WithPgWeb(); // 資料庫網頁客戶端-資料表查詢工具
+
 builder.AddProject<Projects.WebAPI>("webapi");
 
 builder.Build().Run();
