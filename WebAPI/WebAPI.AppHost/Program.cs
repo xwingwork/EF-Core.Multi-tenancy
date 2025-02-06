47a4ccdbbe36dda6805eb1409444005a808ad822
俊霖 <testaurora@hotmail.com>
2025-02-06 10:53:32 +0800

    優化-添加註解跟調整檔案風格

--- WebAPI/WebAPI.AppHost/Program.cs
+++ WebAPI/WebAPI.AppHost/Program.cs
@@ -1,8 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder
-    .AddPostgres("postgres") // 執行個體，包含預設資料庫
-    .WithBindMount("Development/Postgres", "/docker-entrypoint-initdb.d")
+    .AddPostgres("postgres") // 執行個體，包含預設資料庫。https://learn.microsoft.com/zh-tw/dotnet/aspire/database/postgresql-entity-framework-integration?tabs=dotnet-cli
+    .WithBindMount("Development/Postgres", "/docker-entrypoint-initdb.d") // Seed data https://learn.microsoft.com/en-us/dotnet/aspire/database/seed-database-data?tabs=sql-server#database-seeding-examples
     .WithPgAdmin() // 執行個體網頁客戶端-管理工具
     .WithPgWeb(); // 資料庫網頁客戶端-資料表查詢工具
 
--- WebAPI/WebAPI.Integration.Tests/SingletonTests.cs
+++ WebAPI/WebAPI.Integration.Tests/SingletonTests.cs
@@ -3,38 +3,37 @@ using Aspire.Hosting.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 
-namespace WebAPI.Integration.Tests
+namespace WebAPI.Integration.Tests;
+
+public class SingletonTests
 {
-    public class SingletonTests
+    // 參考微軟範例 https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit#explore-the-test-project
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCodeAsync()
     {
-        // 參考微軟範例 https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit#explore-the-test-project
-        [Fact]
-        public async Task GetWebResourceRootReturnsOkStatusCodeAsync()
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder
+            .CreateAsync<Projects.WebAPI_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
-            // Arrange
-            var appHost = await DistributedApplicationTestingBuilder
-                .CreateAsync<Projects.WebAPI_AppHost>();
-            appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-            {
-                clientBuilder.AddStandardResilienceHandler();
-            });
-            // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
 
-            await using var app = await appHost.BuildAsync();
-            var resourceNotificationService = app.Services
-                .GetRequiredService<ResourceNotificationService>();
-            await app.StartAsync();
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services
+            .GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
 
-            // Act
-            var httpClient = app.CreateHttpClient("webapi");
-            httpClient.DefaultRequestHeaders.Add("TenantId", "a0ac");
-            await resourceNotificationService
-                .WaitForResourceAsync("webapi", KnownResourceStates.Running) // 等待依賴項目運行
-                .WaitAsync(TimeSpan.FromSeconds(30));
-            var response = await httpClient.GetAsync("/todos");
+        // Act
+        var httpClient = app.CreateHttpClient("webapi");
+        httpClient.DefaultRequestHeaders.Add("TenantId", "a0ac");
+        await resourceNotificationService
+            .WaitForResourceAsync("webapi", KnownResourceStates.Running) // 等待依賴項目運行
+            .WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/todos");
 
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
